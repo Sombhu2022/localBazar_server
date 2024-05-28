ab0350de94ee78603f79067be638aebf0eaf1fd5
Sombhu2022 <sombhudas93@gmail.com>
2024-05-28 10:54:44 +0530

    final push

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -2,6 +2,11 @@ import { Product } from "../models/productsModel.js";
 import { Shop } from "../models/shopModel.js";
 import { User } from "../models/userModel.js"
 
+import { imageDestroy } from "../utils/imageDestroy.js";
+
+
+import { imageUploader } from "../utils/imageUploder.js";
+
 export const createShop = async (req, res) => {
     try {
 
@@ -16,10 +21,24 @@ export const createShop = async (req, res) => {
         //     }
         // }
 
-        const { shopName, location } = req.body;
-        console.log(shopName, location);
 
+        const { shopName, shopImage, pin, city, country, customerCareNumber } = req.body;
+
+        console.log(req.body);
+        const location = {
+            country,
+            city,
+            pin,
+
+
+        }
+        console.log(location);
         // upload image
+        const data = await imageUploader(shopImage)
+        console.log(data?.url, data?.public_id);
+
+        // const 
+
         // shopImage
 
         const newShop = await Shop.create({
@@ -27,10 +46,12 @@ export const createShop = async (req, res) => {
             location,
             customerCareNumber,
             owner: req.user._id,
-            // shopImage: {
-            //     url: ,
-            //     public_id: 
-            // },
+            shopImage: {
+
+                url: data?.url,
+                public_id: data?.public_id
+
+            },
         })
 
         res.status(200).json({
@@ -74,6 +95,19 @@ export const getShopDetails = async (req, res) => {
         console.log(shopid);
 
         const shop = await Shop.findById(shopid)
+
+            .populate('owner')
+            .populate('products')
+            .populate(
+                {
+                    path: "reviews",
+                    populate: {
+                        path: "user",
+                        model: "User"
+                    }
+                }
+            )
+
 
         if (!shop) {
             return res.status(200).json({
@@ -243,92 +277,6 @@ export const postShopReview = async (req, res) => {
         const user = req.user;
         const { shopId } = req.params;
         const { rating, message } = req.body
-
-        const  shop = await Shop.findById(shopId)
-        .populate('owner')
-        .populate('products')
-        .populate('reviews.user')
-        .populate(
-            {
-                path: "reviews",
-                populate: {
-                    path: "user",
-                    model: "User"
-                }
-            }
-        )
-
-        console.log(user._id);
-        console.log("log",reviewPresent(shop , user._id));
-
-        if (reviewPresent(shop , user._id)) {
-
-            const existReview = reviewPresent(shop , user._id)
-
-            // console.log("review " , review);
-
-            if (rating)  existReview.rating = rating
-            if (message) existReview.message = message
-            
-        } else {
-            shop?.reviews.push(
-                {
-                    user: user._id,
-                    rating,
-                    message
-                }
-            )
-            
-        }
-    
-        const totalReview = shop?.reviews.length
-        
-        let totalRating =0;
-        shop.reviews?.map((ele)=>{
-            totalRating += ele.rating
-        })
-
-        shop.ratings = totalRating / totalReview
-        
-        // save finding product with out schema varification 
-        await shop.save({ validateBeforeSave: false })
-
-        // this call populate maping ...  
-       
-        // console.log(data);
-        res.status(200).json({
-            message: "review add",
-            shop
-        })
-
-    } catch (error) {
-        console.log(error);
-        res.status(400).json({
-            message: "review not add",
-            error
-        })
-
-    }
-}
-
-const reviewPresent = (shop, userid) => {
-    return shop?.reviews?.find((ele) => {
-        if (String(ele?.user?._id) === String(userid)) {
-            console.log("ok");
-            return ele
-        } else {
-            return ""
-        }
-    })
-}
-
-
-export const postShopReview = async (req, res) => {
-    try {
-        const user = req.user;
-        const { shopId } = req.params;
-        const { rating, message } = req.body
-
         const shop = await Shop.findById(shopId)
             .populate('owner')
             .populate('products')
@@ -355,6 +303,7 @@ export const postShopReview = async (req, res) => {
             if (rating) existReview.rating = rating
             if (message) existReview.message = message
 
+
         } else {
             shop?.reviews.push(
                 {
@@ -364,12 +313,14 @@ export const postShopReview = async (req, res) => {
                 }
             )
 
+
         }
 
         const totalReview = shop?.reviews.length
 
         let totalRating = 0;
         shop.reviews?.map((ele) => {
+
             totalRating += ele.rating
         })
 
@@ -379,7 +330,6 @@ export const postShopReview = async (req, res) => {
         await shop.save({ validateBeforeSave: false })
 
         // this call populate maping ...  
-
         // console.log(data);
         res.status(200).json({
             message: "review add",
@@ -395,6 +345,7 @@ export const postShopReview = async (req, res) => {
 
     }
 }
+
 
 
 export const getShopByShopName = async (req, res) => {
@@ -484,3 +435,4 @@ if(productName){
         })
     }
 }
+
